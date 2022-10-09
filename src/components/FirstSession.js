5de4733c5b2ad30b71da01d9b4c069eef50284bc
Tayanne Bencardino <tayannebencardino@gmail.com>
2022-10-09 14:03:12 +0100

    Add first session to the page

---
+++ src/components/FirstSession.js
@@ -0,0 +1,17 @@
+import "./FirstSession.css"
+
+export const FirstSession = () => {
+    return (
+        <div>
+            <div className="container"></div>
+            <img className="avatar" src={require("../images/avatar.png")} alt="tayanne-avatar"/>
+            <div className="box">
+                <h1>Hi, I'm <span className="name">Tayanne Bencardino</span>. And I am <span className="ocupation">Web Developer</span>!</h1>
+                
+            </div>
+            
+        </div>
+
+        
+    )
+}
