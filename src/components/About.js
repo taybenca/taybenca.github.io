0ab9f2c22a7bf9576772f12dcf6362649051271e
Tayanne Bencardino <tayannebencardino@gmail.com>
2022-10-10 15:11:47 +0100

    make about and title in center for width 39 390px

--- src/components/About.js
+++ src/components/About.js
@@ -3,8 +3,8 @@ import "./About.css"
 
 export const About = () => {
     return (
-        <div className="about" id="about">
-            <p>A Full Stack Software Developer who uses Ruby, JavaScript and React to write web applications. </p>
-        </div>
+        <session className="about" id="about">
+            <p>I became a Software Developer as I believe technology provides a digital canvas for innovative ideas and solutions to problems. Creating something that improves the lives of others or sparks joy is my biggest motivation. I like collaborating on projects that share my desire for innovation and determination to impact society positively. </p>
+        </session>
     )
 }
