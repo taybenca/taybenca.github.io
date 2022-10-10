5d50187f523192b4c74e35503299747a0bee5e27
Tayanne Bencardino <tayannebencardino@gmail.com>
2022-10-10 16:58:39 +0100

    Add skills

--- src/components/FirstSession.js
+++ src/components/FirstSession.js
@@ -1,5 +1,6 @@
 import "./FirstSession.css"
 import { NavBar } from "./NavBar"
+import { Skills } from "./Skills"
 
 export const FirstSession = () => {
     return (
@@ -12,6 +13,7 @@ export const FirstSession = () => {
                 And I am <span className="ocupation">Web Developer</span>!</h1>
                 <p>I believe technology provides a digital canvas for innovative ideas and solutions to problems.
                 Creating something that improves the lives of others or sparks joy is my biggest motivation.</p>
+                <Skills/>
             </div>
         </session>
     )
---
+++ src/components/Skills.js
@@ -0,0 +1,18 @@
+import "./Skills.css"
+
+export const Skills = () => {
+    return (
+        <div className="skills">
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/ruby/ruby-original.svg" alt="Ruby"/>
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" alt="javascript"/>
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg" alt="npm"/>
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" alt="nodejs" />
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" alt="react"/>
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" alt="css" />
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" alt="html" />
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg" alt="mongo db" />
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg" alt="postgre sql" />
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/heroku/heroku-original-wordmark.svg" alt="heroku"/>
+        </div>
+    )
+}
