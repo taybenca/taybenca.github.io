557a467b6ea8afd5a942502f91583d434d9b60e8
Tayanne Bencardino <tayannebencardino@gmail.com>
2022-10-10 11:07:20 +0100

    hide navbar when screen is smaller than 850px

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,14 +3,16 @@ import "./NavBar.css"
 
 export const NavBar = () => {
     return (
-        <div className="topnav">
-            <ul>
-                <li><a href="#home">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#projects">Projects</a></li>
-                <li><a href="#blog">Blog</a></li>
-                <li><a href="#contact">Contact</a></li>
-            </ul>
+        <div>
+            <nav className="topnav">
+                <ul>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#projects">Projects</a></li>
+                    <li><a href="#blog">Blog</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </nav>
         </div>
     )
 }
